603598891f872d4f332ab5f46ff9dc9f34933d35
Meghatania03 <141716710+Meghatania03@users.noreply.github.com>
2025-10-06 16:10:54 +0600

    deleted district options in homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -307,73 +307,7 @@
                 <option value="sheep">Sheep</option>
                 <option value="camel">Camel</option>
             </select>
-            <select name="location">
-                <option value="">All Locations</option>
-                <option value="Dhaka">Dhaka</option>
-                <option value="Faridpur">Faridpur</option>
-                <option value="Gazipur">Gazipur</option>
-                <option value="Gopalganj">Gopalganj</option>
-                <option value="Jamalpur">Jamalpur</option>
-                <option value="Kishoreganj">Kishoreganj</option>
-                <option value="Madaripur">Madaripur</option>
-                <option value="Manikganj">Manikganj</option>
-                <option value="Munshiganj">Munshiganj</option>
-                <option value="Mymensingh">Mymensingh</option>
-                <option value="Narayanganj">Narayanganj</option>
-                <option value="Narsingdi">Narsingdi</option>
-                <option value="Netrokona">Netrokona</option>
-                <option value="Rajbari">Rajbari</option>
-                <option value="Shariatpur">Shariatpur</option>
-                <option value="Sherpur">Sherpur</option>
-                <option value="Tangail">Tangail</option>
-                <option value="Bogura">Bogura</option>
-                <option value="Joypurhat">Joypurhat</option>
-                <option value="Naogaon">Naogaon</option>
-                <option value="Natore">Natore</option>
-                <option value="Nawabganj">Nawabganj</option>
-                <option value="Pabna">Pabna</option>
-                <option value="Rajshahi">Rajshahi</option>
-                <option value="Sirajgonj">Sirajgonj</option>
-                <option value="Dinajpur">Dinajpur</option>
-                <option value="Gaibandha">Gaibandha</option>
-                <option value="Kurigram">Kurigram</option>
-                <option value="Lalmonirhat">Lalmonirhat</option>
-                <option value="Nilphamari">Nilphamari</option>
-                <option value="Panchagarh">Panchagarh</option>
-                <option value="Rangpur">Rangpur</option>
-                <option value="Thakurgaon">Thakurgaon</option>
-                <option value="Barguna">Barguna</option>
-                <option value="Barishal">Barishal</option>
-                <option value="Bhola">Bhola</option>
-                <option value="Jhalokati">Jhalokati</option>
-                <option value="Patuakhali">Patuakhali</option>
-                <option value="Pirojpur">Pirojpur</option>
-                <option value="Bandarban">Bandarban</option>
-                <option value="Brahmanbaria">Brahmanbaria</option>
-                <option value="Chandpur">Chandpur</option>
-                <option value="Chattogram">Chattogram</option>
-                <option value="Cumilla">Cumilla</option>
-                <option value="Cox's Bazar">Cox's Bazar</option>
-                <option value="Feni">Feni</option>
-                <option value="Khagrachari">Khagrachari</option>
-                <option value="Lakshmipur">Lakshmipur</option>
-                <option value="Noakhali">Noakhali</option>
-                <option value="Rangamati">Rangamati</option>
-                <option value="Habiganj">Habiganj</option>
-                <option value="Maulvibazar">Maulvibazar</option>
-                <option value="Sunamganj">Sunamganj</option>
-                <option value="Sylhet">Sylhet</option>
-                <option value="Bagerhat">Bagerhat</option>
-                <option value="Chuadanga">Chuadanga</option>
-                <option value="Jashore">Jashore</option>
-                <option value="Jhenaidah">Jhenaidah</option>
-                <option value="Khulna">Khulna</option>
-                <option value="Kushtia">Kushtia</option>
-                <option value="Magura">Magura</option>
-                <option value="Meherpur">Meherpur</option>
-                <option value="Narail">Narail</option>
-                <option value="Satkhira">Satkhira</option>
-            </select>
+           <input type="text" name="location" placeholder="Location (e.g., Dhaka)">
             <input type="number" name="max_price" placeholder="Max Price (৳)">
             <button type="submit" class="btn btn-primary">Search</button>
         </form>
