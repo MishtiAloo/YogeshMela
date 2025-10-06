16b520ad8d42cff6c0d67f873fda6f7c976549f9
dipshekhor <datta2107047@stud.kuet.ac.bd>
2025-10-06 13:16:24 +0600

    Add listings page with filters and pagination

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,13 @@ use App\Http\Controllers\BuyerDashboardController;
 use App\Http\Controllers\AdminDashboardController;
 use App\Http\Controllers\SellerDashboardController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ListingController;
 use App\Http\Controllers\UserController;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/listings', [ListingController::class, 'index'])->name('listings.index');
 
 // Auth routes (login/logout)
 Route::middleware(['web'])->group(function () {
